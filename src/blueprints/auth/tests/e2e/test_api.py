2ef6107ff5f8095d1d9556254d0101282620f4ff
Juan Carlos Jose Camacho <juan.jose@oyster.io>
2022-09-01 12:26:11 -0500

    Improve e2e api tests

--- src/blueprints/auth/tests/e2e/test_api.py
+++ src/blueprints/auth/tests/e2e/test_api.py
@@ -1,13 +1,11 @@
-import pytest
 import src.config as config
 import requests
 
 
-@pytest.mark.usefixtures('in_memory_db')
 def test_register():
     url = config.get_api_url()
     r = requests.post(
         f'{url}/auth/register',
-        json={"username":"david7", "password":"abc123", "created_at": "2022-12-07"}
+        json={"username":"test", "password":"abc123", "created_at": "2022-12-07"}
     )
     assert r.status_code == 200
--- src/blueprints/core/tests/e2e/test_api.py
+++ src/blueprints/core/tests/e2e/test_api.py
@@ -1,30 +1,27 @@
-import pytest
 import src.config as config
 import requests
 
 
-@pytest.mark.usefixtures('in_memory_db')
 def test_register():
     url = config.get_api_url()
 
     # Register user
     requests.post(
         f'{url}/auth/register',
-        json={"username": "test", "password": "test", "created_at": "2022-12-07"}
+        json={"username": "test", "password": "abc123", "created_at": "2022-12-07"}
     )
 
     # Login
     r = requests.post(
         f'{url}/auth/login',
-        json={"username":"test", "password":"test"}
+        json={"username":"test", "password":"abc123"}
     )
     token = r.json()['access_token']
 
     headers = {"Authorization": f"Bearer {token}"}
     # Create todo
-    r = requests.post(
-        f'{url}/add_todo',
-        headers=headers,
-        json={"name":"agua", "user_id": 1, "created_at": "2022-12-07"}
+    r = requests.get(
+        f'{url}/',
+        headers=headers
     )
-    assert r.status_code == 201
+    assert r.status_code == 200
